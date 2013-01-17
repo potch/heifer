50486b1d0999f2203975cb86376f0c3e6dff5926
Matt Claypotch <thepotch@gmail.com>
2013-01-16 22:36:01 -0800

    easier invocation

--- heifer.node.js
+++ heifer.node.js
@@ -1,23 +1,26 @@
 var _ = require('underscore');
 
-if (!process.stdin) {
+var spawn = require('child_process').spawn;
 
-    console.log('no input!');
+var url = process.argv[2];
+
+console.log(process.argv);
+
+var doc = '';
+
+function append(content) {
+    doc += content + '\n';
+}
+
+if (!url) {
+
+    console.log('no URL given!');
 
 } else {
 
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
+    var args = process.argv.slice(3);
 
-    var input = '';
-
-    process.stdin.on('data', function (chunk) {
-      input += chunk;
-    });
-
-    process.stdin.on('end', function () {
-      analyze(input);
-    });
+    yslow(url, args, analyze);
 
 }
 
@@ -34,12 +37,70 @@ function analyze(data) {
             url: decodeURIComponent(f.url)
         };
     });
-    console.log('<style>tr { line-height: 1.4em; } tr:nth-child(2n+1) { background:#eee; }</style>');
-    console.log('<table style="width: 100%;font-family:monospace">');
-    console.log('<tr><th>URL<th>type<th>size');
-    _.sortBy(files, 'size').reverse().forEach(function(f) {
-        console.log('<tr><td>' + [f.url,f.type,f.size].join('<td>'));
+
+    append('<style>table { font-family:monospace} tr { line-height: 1.4em; } tr:nth-child(2n+1) { background:#eee; } td { padding: 0 8px; }</style>');
+
+    append(header('Resource Breakdown'));
+
+    var total = 0;
+    var types = {};
+
+    files.forEach(function(file) {
+        total += file.size;
+        if (types[file.type] === undefined) {
+            types[file.type] = 0;
+        }
+        types[file.type] += file.size;
     });
-    console.log('</table>');
+
+    types = _.map(types, function(v, k) {
+        return [k, v];
+    });
+    types = _.sortBy(types, 1).reverse();
+    types.push(['total', total]);
+
+    append(table(types, _.identity));
+
+    append(header('Resources Loaded'));
+
+    append(table(_.sortBy(files, 'size').reverse(), function(row) {
+        return [row.url, row.type, row.size];
+    }, ['URL', 'type', 'size']));
+
+    console.log(doc);
 }
 
+function header(txt) {
+    return '<h1>' + txt + '</h1>';
+}
+
+function table(data, rowFunc, header) {
+    var out = '<table>';
+    if (header) {
+        out += '<tr><th>' + header.join('</th><th>') + '</th></tr>';
+    }
+    data.forEach(function(row) {
+        out += '<tr><td>' + rowFunc(row).join('</td><td>') + '</td></tr>';
+    });
+    out += '</table>';
+    return out;
+}
+
+function yslow(url, args, cb) {
+    var output = '';
+    var error = '';
+
+    args = ['yslow.js', '-icomps'].concat(args).concat([url]);
+    var job = spawn('phantomjs', args);
+
+    job.stdout.on('data', function(data) {
+        output += data;
+    });
+    job.stderr.on('data', function(data) {
+        error += data;
+    });
+    job.on('exit', function() {
+        cb(output, error);
+    });
+
+}
